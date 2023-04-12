18903959c41b9752d0c3470b2a1f9e1b040b6850
owusuvictorjnr <owusuvictor80@gmail.com>
2023-04-12 04:36:30 +0000

    Initial config

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <header>vitech solutions</header>
+
+      <main></main>
+
+      <footer>all right reserved</footer>
+    </div>
+  )
+}
+
+export default App
