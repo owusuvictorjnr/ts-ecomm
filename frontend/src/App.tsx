2c24496d546dcfa5576d25d4d243cd07f41d8dc4
owusuvictorjnr <owusuvictor80@gmail.com>
2023-04-12 10:57:04 +0000

    Feature: Rendering product on the screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,26 @@
 import './App.css'
+import { sampleProducts } from './data'
 
 function App() {
   return (
     <div>
       <header>vitech solutions</header>
 
-      <main></main>
+      <main>
+        <ul>
+          {sampleProducts.map((product) => (
+            <li key={product.slug}>
+              <img
+                src={product.image}
+                alt={product.name}
+                className="product-image"
+              />
+              <h2>{product.name}</h2>
+              <p>&#8373;{product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
 
       <footer>all right reserved</footer>
     </div>
