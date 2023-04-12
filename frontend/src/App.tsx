2ff5f376223db02c6759a15abd50331b5546d0ab
owusuvictorjnr <owusuvictor80@gmail.com>
2023-04-12 16:33:17 +0000

    Feature: Create navbar and wrap container, row and column around samle product in main

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,47 @@
-import './App.css'
+import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap'
 import { sampleProducts } from './data'
 
 function App() {
   return (
-    <div>
-      <header>vitech solutions</header>
+    <div className="d-flex flex-column vh-100">
+      <header>
+        <Navbar bg="dark" variant="dark" expand="lg">
+          <Container>
+            <Navbar.Brand>vitech solutions</Navbar.Brand>
+          </Container>
+
+          <Nav>
+            <a href="/cart" className="nav-link">
+              cart
+            </a>
+            <a href="/signin" className="nav-link">
+              sign in
+            </a>
+          </Nav>
+        </Navbar>
+      </header>
 
       <main>
-        <ul>
-          {sampleProducts.map((product) => (
-            <li key={product.slug}>
-              <img
-                src={product.image}
-                alt={product.name}
-                className="product-image"
-              />
-              <h2>{product.name}</h2>
-              <p>&#8373;{product.price}</p>
-            </li>
-          ))}
-        </ul>
+        <Container className="mt-3">
+          <Row>
+            {sampleProducts.map((product) => (
+              <Col key={product.slug} sm={6} md={4} lg={3}>
+                <img
+                  src={product.image}
+                  alt={product.name}
+                  className="product-image"
+                />
+                <h2>{product.name}</h2>
+                <p>&#8373;{product.price}</p>
+              </Col>
+            ))}
+          </Row>
+        </Container>
       </main>
 
-      <footer>all right reserved</footer>
+      <footer>
+        <div className="text-center">all right reserved</div>
+      </footer>
     </div>
   )
 }
