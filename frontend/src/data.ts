c1ade9b4ba82b04797430e9950407defb27b7c02
owusuvictorjnr <owusuvictor80@gmail.com>
2023-04-12 11:04:56 +0000

    Feature: Product data

---
+++ frontend/src/data.ts
@@ -0,0 +1,55 @@
+import { Product } from './types/Product'
+
+export const sampleProducts: Product[] = [
+  {
+    name: 'Short sleeves',
+    slug: 'short-sleevs',
+    category: 'short-sleevs',
+    image: '../images/pic1.jpg',
+    price: 230,
+    countInStock: 20,
+    brand: 'African Print',
+    rating: 4.5,
+    numReviews: 29,
+    description: 'high quality short sleeve',
+  },
+
+  {
+    name: 'Long sleeves',
+    slug: 'long-sleevs',
+    category: 'long-sleevs',
+    image: '../images/pic2.jpg',
+    price: 100,
+    countInStock: 10,
+    brand: 'African Blue Print',
+    rating: 4.5,
+    numReviews: 19,
+    description: 'high quality long sleeve',
+  },
+
+  {
+    name: 'Smock',
+    slug: 'smock',
+    category: 'Northen Wear',
+    image: '../images/p3.jpg',
+    price: 330,
+    countInStock: 30,
+    brand: 'African Print',
+    rating: 4.5,
+    numReviews: 29,
+    description: 'high quality smock',
+  },
+
+  {
+    name: 'Tie and dye',
+    slug: 'tie-and-dye',
+    category: 'ashanti wear',
+    image: '../images/pic4.jpg',
+    price: 200,
+    countInStock: 20,
+    brand: 'African Print',
+    rating: 4.5,
+    numReviews: 29,
+    description: 'high quality sleeve',
+  },
+]
